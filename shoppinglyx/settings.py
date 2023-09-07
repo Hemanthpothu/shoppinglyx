571a7d52eb1a8d252179f201f742b17ae94ca040
Hemanth Pothu <56197063+Hemanthpothu@users.noreply.github.com>
2023-09-07 20:00:20 +0530

    added

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -75,22 +75,24 @@ WSGI_APPLICATION = 'shoppinglyx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'UkQJkEAVgxx5yjIMlwDP',
-#         'HOST': 'containers-us-west-94.railway.app',
-#         'PORT': '6595',
-#     }
-# }
 DATABASES = {
-    "default":{
-        'ENGINE': 'django.db.backends.sqlite3',
-        "NAME" : BASE_DIR/"db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'UkQJkEAVgxx5yjIMlwDP',
+        'HOST': 'containers-us-west-94.railway.app',
+        'PORT': '6595',
     }
 }
+
+
+# DATABASES = {
+#     "default":{
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         "NAME" : BASE_DIR/"db.sqlite3",
+#     }
+# }
 
 
 
